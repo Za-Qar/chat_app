ce89654b3e33cade04ea6a648982685de387d146
= <za.qa@outlook.com>
2021-01-28 14:23:13 +0000

    fixes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const http = require("http").Server(app);
+const PORT = 3000;
 const io = require("socket.io")(http, {
   pingTimeout: 60000,
 });
@@ -27,6 +28,39 @@ io.on("connection", (socket) => {
   });
 });
 
-http.listen(3000, () => {
+http.listen(PORT, () => {
   console.log("listening on port 3000");
 });
+
+// const express = require("express");
+// const app = express();
+// const http = require("http").Server(app);
+// const io = require("socket.io")(http, {
+//   pingTimeout: 60000,
+// });
+// const path = require("path");
+
+// //Serve public directory
+// app.use(express.static(path.join(__dirname, "public")));
+
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, +"public/index.html"));
+// });
+
+// io.on("connection", (socket) => {
+//   console.log("a user connected");
+
+//   socket.on("disconnect", () => {
+//     console.log("user disconnected");
+//   });
+
+//   socket.on("message", (message) => {
+//     console.log("message: " + message.content);
+//     //Broadcast the message to everyone
+//     io.emit("message", message);
+//   });
+// });
+
+// http.listen(3000, () => {
+//   console.log("listening on port 3000");
+// });
